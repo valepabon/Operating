d23ecab9bdc428712fe3812b7628b9c2266fd6a8
valepabon <125760572+valepabon@users.noreply.github.com>
2025-02-25 10:15:48 -0500

    Update SJF_P.java

--- src/ur_os/SJF_P.java
+++ src/ur_os/SJF_P.java
@@ -4,39 +4,34 @@ import java.util.PriorityQueue;
 
 public class SJF_P extends Scheduler {
 
-    // Cola de prioridad basada en la ráfaga de CPU más corta
     private PriorityQueue<Process> colaListos;
 
-    // Constructor
     SJF_P(OS os) {
         super(os);
-        colaListos = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getRemainingCPUBurst(), p2.getRemainingCPUBurst()));
+        colaListos = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getRemainingTimeInCurrentBurst(), p2.getRemainingTimeInCurrentBurst()));
     }
 
-    // Método para seleccionar el siguiente proceso a ejecutar
     @Override
     public void getNext(boolean cpuEmpty) {
         if (!colaListos.isEmpty() && cpuEmpty) {
-            Process p = colaListos.poll(); // Sacar el proceso con menor ráfaga
-            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p); // Asignar a la CPU
+            Process p = colaListos.poll();
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, p);
         }
     }
 
-    // Manejo de nuevos procesos que entran al sistema
     @Override
     public void newProcess(boolean cpuEmpty) {
         if (!processes.isEmpty()) {
-            colaListos.addAll(processes); // Agregar procesos nuevos a la cola de prioridad
-            processes.clear(); // Limpiar la lista de procesos listos, ya están en la cola de prioridad
-            getNext(cpuEmpty); // Verificar si se puede ejecutar un nuevo proceso
+            colaListos.addAll(processes);
+            processes.clear();
+            getNext(cpuEmpty);
         }
     }
 
-    // Manejo de procesos que retornan de I/O
     @Override
     public void IOReturningProcess(boolean cpuEmpty) {
         if (!processes.isEmpty()) {
-            colaListos.addAll(processes); // Agregar procesos que regresan de I/O
+            colaListos.addAll(processes);
             processes.clear();
             getNext(cpuEmpty);
         }
